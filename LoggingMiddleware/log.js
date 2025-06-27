e1c7d8ad99867a34bdc0f372581cf017f4d55664
sai venkat <saivenkatkallepalli@gmail.com>
2025-06-27 11:11:30 +0530

    created logging middleware

---
+++ LoggingMiddleware/log.js
@@ -0,0 +1,21 @@
+const LOGGING=async function sendLoginData(stack,level,package,message,) {
+  const data = {
+    stack:stack,
+    level:level,
+    package:package,
+    message:message
+  };
+
+  const response = await fetch("http://20.244.56.144/evaluation-service/logs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  const result = await response.json();
+  console.log(result);
+}
+
+module.exports=LOGGING;
