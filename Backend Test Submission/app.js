b93a89e12c9d9e53e7a107a3f012a0ef7e91a280
sai venkat <saivenkatkallepalli@gmail.com>
2025-07-06 00:25:23 +0530

    added index.html file in backend

--- Backend Test Submission/app.js
+++ Backend Test Submission/app.js
@@ -11,6 +11,17 @@ app.use(express.json());
 
 app.use('/', urlRoutes);
 
+// Serve the static files (HTML, CSS, JS)
+app.use(express.static("public"));
+// to display (serve) html ( to make sure that the server is running when HOSTED)
+app.get(["/", "/api"], (req, res) => {
+  try {
+    res.sendFile(__dirname + "/public/index.html");
+  } catch (e) {
+    console.log("erorrrrr", e);
+  }
+});
+
 mongoose.connect('mongodb://localhost:27017/urlshortener')
 .then(() => {
   console.log('MongoDB connected');
